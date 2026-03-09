48e3d4b4c820ac6e74fce55d8899bc9e8d03d129
Saurabh Nage <102867753+Saurabhnage@users.noreply.github.com>
2026-03-10 04:41:40 +0530

    Set up Express server for Ping2Pay API

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,14 @@
+const express = require("express");
+require("dotenv").config();
 
+require("./bot");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Ping2Pay API running");
+});
+
+app.listen(3000, () => {
+  console.log("Ping2Pay server running on port 3000");
+});
