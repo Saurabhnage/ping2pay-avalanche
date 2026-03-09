b68520be7897d58f8e64509ee1924bfc171bba0c
Saurabh Nage <102867753+Saurabhnage@users.noreply.github.com>
2026-03-10 04:40:46 +0530

    Update wallet.js

--- backend/wallet.js
+++ backend/wallet.js
@@ -1,1 +1,13 @@
+const { ethers } = require("ethers");
 
+function createWallet() {
+
+  const wallet = ethers.Wallet.createRandom();
+
+  return {
+    address: wallet.address,
+    privateKey: wallet.privateKey
+  };
+}
+
+module.exports = createWallet;
