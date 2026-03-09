a7272ffe710926617c2598fcaebcfeafd1854286
Saurabh Nage <102867753+Saurabhnage@users.noreply.github.com>
2026-03-10 04:40:07 +0530

    Configure Hardhat for Avalanche Fuji network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,1 +1,14 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
+module.exports = {
+
+  solidity: "0.8.19",
+
+  networks: {
+    avalancheFuji: {
+      url: process.env.AVALANCHE_RPC,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
+};
